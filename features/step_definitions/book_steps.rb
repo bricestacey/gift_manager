cd0f89c0285061aa12e792e5e13ddc916a6560d5
Brice Stacey <bricestacey@gmail.com>
2011-06-24 17:05:33 -0400

    Add step definitions for checking copy and charge counts

--- features/step_definitions/book_steps.rb
+++ features/step_definitions/book_steps.rb
@@ -5,3 +5,16 @@ end
 When /^I fill in "ISBN" with the ISBN of a book without a circulation$/ do
   When %{I fill in "ISBN" with "#{ISBN_BOOK_NOT_CIRCULATED}"}
 end
+
+Then /^I should see that there is at least one copy$/ do
+  Then %{I should see /Copies [^0]/}
+end
+
+Then /^I should see that there is at least one circulation$/ do
+  Then %{I should not see "Charges 0"}
+end
+
+Then /^I should see that it has not circulated$/ do
+  Then %{I should see "Charges 0"}
+end
+
